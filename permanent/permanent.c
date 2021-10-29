41bed0e9298869822a55db6db39569cc2108b907
cassmasschelein <cassandramasschelein@gmail.com>
2021-10-29 13:25:12 -0400

    updated working combinatoric algorithm, all tests passed

--- permanent/permanent.c
+++ permanent/permanent.c
@@ -4,6 +4,7 @@
 
 /* Python C header. */
 #include <Python.h>
+#include <stdbool.h>
 
 
 /* NumPy C headers. */
@@ -32,85 +33,70 @@ void swap2(uint64_t *perm, uint64_t i, uint64_t j)
     perm[j] = temp;
 }
 
-void re_sort(uint64_t *const curr_perm, uint64_t start, uint64_t length)
-// Re-sort the set to be in ascending order - this occurs after you have swapped adjacent values
-{
-    uint64_t i = start;
-    uint64_t j = length - 1;
-    while (i < j)
-    {
-        swap2(curr_perm, i, j);
-        i++;
-        j--;
-    }
-}
-
-
-void init_perm(const uint64_t N, const uint64_t M, uint64_t *the_curr_set, uint64_t *the_next_set, uint64_t *the_inv_set)
+void init_perm(const uint64_t N, const uint64_t M, uint64_t *the_fact_set, uint64_t *the_perm_set, uint64_t *the_inv_set)
 {
     // generate the set {1, 2, .., N} to permute
     // update all relevent sets we must keep track up in order to siplify generation of next permutation
     // update values pointed to by respective array pointers
     // first set is in ascending order to ensure no smaller permutation is possible
 
-    uint64_t k = M;
-    uint64_t u = N - 1;
-    if (k < u)
-    {
-        u = k;
-    }
-    
     for (uint64_t i = 0; i < N; i++)
     {
-        the_curr_set[i] = 0;
-        the_next_set[i] = i;
+        the_fact_set[i + 1] = 0;
+        the_perm_set[i] = i;
         the_inv_set[i] = i;
     }
 }
 
 
-void gen_next_perm(uint64_t *const curr_perm, uint64_t *const next_perm, uint64_t *const inv_perm, const uint64_t rows, const uint64_t cols)
+bool gen_next_perm(uint64_t *const falling_fact, uint64_t *const perm_, uint64_t *const inv_perm, const uint64_t rows, const uint64_t cols, const uint64_t u_)
 {
     /* use the current permutation to check for next permutation lexicographically, if it exists
     update the curr_array by swapping the leftmost changed element (at position i < k).
     Replace the elements up to position k by the smallest element lying to the right of i.
     Do not put remaining k, .., n - 1 positions in ascending order to improve efficiency
     has time complexity O(n) in the worst case */
-    uint64_t i = rows - 1;
-    uint64_t m1 = cols - i - 1;
-    while (curr_perm[i] == m1)
-    // begin update of curr_perm and generate next permutation
+    int64_t i = rows - 1;
+    int64_t m1 = cols - i - 1;
+    while (falling_fact[i + 1] == m1)
+    // begin update of falling_fact and generate next permutation
     {
-        curr_perm[i] = 0;
+        falling_fact[i + 1] = 0;
         ++m1;
         --i;
     }
-    ++curr_perm[i];
+
+    if (i == -1)
+    {
+        return false;
+        // we have already generated all permutations
+    }
+    ++falling_fact[i + 1];
     // find smallest element p[i] < p[j] that lies to the right of pos i
-    uint64_t z = next_perm[i];
+    uint64_t z = perm_[i];
     // update the permutation
     do 
     {
         ++z;
     } while (inv_perm[z] <= i);
     const uint64_t j = inv_perm[z];
-    swap2(next_perm, i, j);
-    swap2(inv_perm, next_perm[i], next_perm[j]);
+    swap2(perm_, i, j);
+    swap2(inv_perm, perm_[i], perm_[j]);
     ++i;
-    z = 0;
-    uint64_t u_ = cols - 1;
+    uint64_t y = 0;
     while (i < u_)
     {
         // find smallest elements to the right of position i
-        while (inv_perm[z] < i)
+        while (inv_perm[y] < i)
         {
-            ++z;
+            ++y;
         }
-        const uint64_t j = inv_perm[z];
-        swap2(next_perm, i, j);
-        swap2(inv_perm, next_perm[i], next_perm[j]);
+        const uint64_t j = inv_perm[y];
+        swap2(perm_, i, j);
+        swap2(inv_perm, perm_[i], perm_[j]);
         ++i;
     }
+    return true;
 }
 
 
@@ -126,32 +112,61 @@ static PyObject *combinatoric(PyObject *module, PyObject *object)
     /* Return the permanent of the matrix. */
     uint64_t m_rows = PyArray_DIMS(matrix)[0];
     uint64_t n_cols = PyArray_DIMS(matrix)[1];
+    uint64_t sort_up_to;
+    // sort up to position u + 1 where u = min(k, n_cols - 1)
+
+    if (m_rows < n_cols)
+    {
+        sort_up_to = m_rows;
+    }
+    else
+    {
+        sort_up_to = n_cols - 1;
+    }
+
     double sum_permanent = 0.0;
     double prod_permanent = 1.0;
 
 
     // generate a pointer to the set of elements {1, 2, .., N} and determing the position of the leftmost change
-    // store the pointer to the array in variable curr_perm
-    uint64_t curr_perm[128];
-    curr_perm[0] = 0;
-    // sentinal value, thus we need to increment to look in the right place
-    // ++curr_perm;
-    uint64_t next_perm[128];
+    // store the pointer to the array in variable falling_fact
+    uint64_t falling_fact[128];
+    falling_fact[0] = 0;
+    // this value will be used to check if we have generated all permutations - allows i to become -1 in th generator
+    uint64_t perm_[128];
     uint64_t inv_perm[128];
     // inverse permutation is used to simplify the update of the permanent
 
-    init_perm(n_cols, m_rows, curr_perm + 1, next_perm, inv_perm);
-    // while (curr_perm[1] < (n_cols - m_rows + 1))
-    while (curr_perm[1] < (n_cols - m_rows + 1))
+    init_perm(n_cols, m_rows, falling_fact, perm_, inv_perm);
+    // while (falling_fact[1] < (n_cols - m_rows + 1))
+    for (uint64_t i = 0; i < 4; i++)
+            printf("perm_[%" PRIu64 "] = %" PRIu64 "\n", i, perm_[i]);
+
+    bool gen_next_perm();
+    // get value from initialization before trying to generate next perm.
+    for (uint64_t i = 0; i < m_rows; ++i)
+        {
+            prod_permanent *= (ptr[i * n_cols + perm_[i]]);
+        }
+    sum_permanent = prod_permanent;
+    printf("%f", sum_permanent);
+    while (gen_next_perm(falling_fact, perm_, inv_perm, m_rows, n_cols, sort_up_to))
     {
+        printf("We made it into the loop!");
+        prod_permanent = 1.0;
         for (uint64_t i = 0; i < m_rows; ++i)
         {
-            prod_permanent *= (ptr[i * m_rows + curr_perm[i]]);
+            prod_permanent *= (ptr[i * n_cols + perm_[i]]);
         }
 
         sum_permanent += prod_permanent;
-        // generate next permutation (if it exists) lexicographically and update the array pointed to by curr_perm
-        gen_next_perm(curr_perm, next_perm, inv_perm, m_rows, n_cols);
+        printf("%f", sum_permanent);        
+
+        for (uint64_t i = 0; i < 3; i++)
+            printf("perm_[%" PRIu64 "] = %" PRIu64 "\n", i, perm_[i]);
+
+        for (uint64_t i = 0; i < 3; i++)
+            printf("falling_fact[%" PRIu64 "] = %" PRIu64 "\n", i, falling_fact[i + 1]);
     }
     return PyFloat_FromDouble(sum_permanent);
 }
--- permanent/test/test_permanent.py
+++ permanent/test/test_permanent.py
@@ -2,12 +2,18 @@ import numpy as np
 
 import permanent
 
-def test_3by3_permanent():
+def test_2by2_permanent():
+    matrix = np.arange(1, 5, dtype=np.double).reshape(2,2)
+    assert permanent.combinatoric(matrix)==10
 
-    matrix = np.arange(1, 10, dtype = np.double).reshape(3,3)
-    assert permanent.combinatoric(matrix) == 450
+def test_3by3_permanent():
+    matrix = np.arange(1, 10, dtype=np.double).reshape(3,3)
+    assert permanent.combinatoric(matrix)==450
 
 def test_4by4_permanent():
+    matrix = np.arange(1, 17, dtype=np.double).reshape(4,4)
+    assert permanent.combinatoric(matrix)==55456
 
-    matrix = np.arange(1, 17, dtype = np.double).reshape(4,4)
-    assert permanent.combinatoric(matrix) == 55456
+def test_2by3_permanent():
+    matrix = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.double)
+    assert permanent.combinatoric(matrix)==58.0
