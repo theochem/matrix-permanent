d18d90d7bab9d55660f8e104c03c01efe973865a
Michelle Richer <michellericher93@gmail.com>
2024-10-05 16:54:32 -0400

    Add 2nd typename param to result_t to enable returning integers

--- include/permanent/common.h
+++ include/permanent/common.h
@@ -17,23 +17,39 @@ using sgn_t = std::int_fast8_t;
 
 // Static dispatcher for result type
 
-template <typename T, typename = void>
+template <typename Type, typename IntType = void, typename = void>
 struct _result_t;
 
-template <typename T>
-struct _result_t<T, std::enable_if_t<std::is_scalar_v<T>, void>>
+template <typename Type>
+struct _result_t<Type, void,
+                 std::enable_if_t<std::is_scalar_v<Type> && !std::is_integral_v<Type>, void>>
 {
   typedef double type;
 };
 
-template <typename T>
-struct _result_t<std::complex<T>, std::enable_if_t<std::is_scalar_v<T>, void>>
+template <typename Type>
+struct _result_t<std::complex<Type>, void,
+                 std::enable_if_t<std::is_scalar_v<Type> && !std::is_integral_v<Type>, void>>
 {
   typedef std::complex<double> type;
 };
 
-template <typename T>
-using result_t = _result_t<T>::type;
+template <typename Type, typename IntType>
+struct _result_t<Type, IntType,
+                 std::enable_if_t<std::is_integral_v<Type> && std::is_integral_v<IntType>, void>>
+{
+  typedef IntType type;
+};
+
+template <typename Type, typename IntType>
+struct _result_t<std::complex<Type>, std::complex<IntType>,
+                 std::enable_if_t<std::is_integral_v<Type> && std::is_integral_v<IntType>, void>>
+{
+  typedef std::complex<IntType> type;
+};
+
+template <typename Type, typename IntType = void>
+using result_t = _result_t<Type, IntType>::type;
 
 // Compute the parity of an integer
 
